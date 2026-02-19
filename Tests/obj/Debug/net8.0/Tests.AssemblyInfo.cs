6d039956f2cd41d71fcc51c75fe092d90fb52868
Negrojo <70605770+noirouge@users.noreply.github.com>
2026-02-18 22:15:33 -0400

    Parte 4 - Postman Collection y Docker

--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5f26a349c07a577876ed4b559e38b19ae5a20a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a93e85fe093205df58f6968f9ac173643e52e06c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
